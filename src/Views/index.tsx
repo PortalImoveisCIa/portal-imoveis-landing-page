d001d36f98b375156dc734ba848935e6ce908053
Volnei <volnei_junior.d@hotmail.com>
2025-03-20 19:34:42 -0300

    primeira landing page

--- src/Views/index.tsx
+++ src/Views/index.tsx
@@ -5,34 +5,45 @@ import { PiUserCirclePlus } from "react-icons/pi";
 import { BsArrowRight } from "react-icons/bs";
 import { IoHomeOutline } from "react-icons/io5";
 import { IoRocketOutline } from "react-icons/io5";
+import { useSearchParams } from "next/navigation";
 
 export function LandingPage() {
   const icons = [
     {
-      icon: <PiUserCirclePlus size={148} />,
+      icon: <PiUserCirclePlus />,
       text: "Crie sua conta Gratuita",
     },
     {
-      icon: <IoHomeOutline size={148} />,
+      icon: <IoHomeOutline />,
       text: "Cadastre os seus imóveis",
     },
     {
-      icon: <IoRocketOutline size={148} />,
+      icon: <IoRocketOutline />,
       text: "Acelere seus resultados",
     },
   ];
+  const searchParams = useSearchParams();
+  console.log(searchParams.get("environment"));
+
+  const domain =
+    searchParams.get("environment") === "hml" ? "devdashboard" : "dashboard";
 
   return (
     <S.Main>
-      <S.Section isDark>
-        <span>
+      <S.Section $isDark>
+        <span
+          style={{
+            fontSize: "3rem",
+            textAlign: "center",
+            marginBottom: "1rem",
+          }}>
           Portal{" "}
-          <b>
+          <b style={{ fontSize: "3rem" }}>
             Imóveis <b className="comercial-e">&</b> Cia
           </b>
         </span>
 
-        <p>
+        <p style={{ fontSize: "1.5rem" }}>
           O único <b>Portal Imobiliário exclusivo do litoral Catarinense</b> com
           Acelerador de Resultados
         </p>
@@ -40,23 +51,61 @@ export function LandingPage() {
         <RedirectButton
           title="Crie sua conta Grátis"
           icon={<BsArrowRight size={20} />}
-          href="https://dashboard.portalimoveisecia.com.br"
+          href={`https://${domain}.portalimoveisecia.com.br/auth/sign-up`}
         />
       </S.Section>
 
-      <S.Section isDark={false}>
-        <b style={{ fontSize: "2rem", width: "50%" }}>
-          Como functiona o Portal Imóveis <b className="comercial-e">&</b> Cia?
+      <S.Section $isDark={false}>
+        <b style={{ fontSize: "1.9rem" }}>
+          Como funciona o Portal Imóveis <b className="comercial-e">&</b> Cia?
         </b>
 
         <S.IconsRow>
           {icons.map((icon) => (
             <S.IconItem key={icon.text}>
-              {icon.icon}
+              <S.Icon>{icon.icon}</S.Icon>
               <span>{icon.text}</span>
             </S.IconItem>
           ))}
         </S.IconsRow>
+      </S.Section>
+
+      <S.AboutSection $isDark>
+        <S.TextWrapper>
+          <h6>Quem Somos?</h6>
+          <p>
+            A <b>Imóveis&Cia</b> começou há 14 anos como a “
+            <b>Revista Imóveis&Cia</b>”, fundada por Tiago Lemos Stachowski.
+            Após 10 anos de sucesso, Tiago expandiu, transformando a revista em
+            uma agência de marketing digital. Atualmente, carregamos em nosso
+            histórico mais de 80 mil leads qualificados, consolidando nossa
+            posição como uma das maiores e mais completas agências de marketing
+            imobiliário do sul do país. Agora para consolidar ainda mais a nossa
+            marca estamos lançando nosso próprio Portal Imobiliário exclusivo do
+            Litoral Catarinense.
+          </p>
+        </S.TextWrapper>
+        <S.ImageWrapper>
+          <S.StyledImage
+            src="/tiago.png"
+            alt="tiago"
+            width={700}
+            height={500}
+            loading="eager"
+          />
+        </S.ImageWrapper>
+      </S.AboutSection>
+
+      <S.Section>
+        <span className="last">
+          Comece agora mesmo a acelerar seus resultados com o Portal Imóveis
+          <b className="comercial-e"> &</b> Cia
+        </span>
+        <RedirectButton
+          title="Crie sua conta Grátis"
+          icon={<BsArrowRight size={20} />}
+          href={`https://${domain}.portalimoveisecia.com.br/auth/sign-up`}
+        />
       </S.Section>
     </S.Main>
   );
--- src/Views/styles.ts
+++ src/Views/styles.ts
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styled from "styled-components";
 
 export const Main = styled.main`
@@ -6,23 +7,35 @@ export const Main = styled.main`
 `;
 
 export const Section = styled.section<{
-  isDark?: boolean;
+  $isDark?: boolean;
 }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme, isDark }) =>
-    isDark ? theme.colors.black : theme.colors.white};
+  background-color: ${({ theme, $isDark }) =>
+    $isDark ? theme.colors.black : theme.colors.white};
+  color: ${({ theme, $isDark }) =>
+    $isDark ? theme.colors.white : theme.colors.black};
   padding: 2rem 4rem;
-  color: ${({ theme, isDark }) =>
-    isDark ? theme.colors.white : theme.colors.black};
   font-size: 2.75rem;
+  word-wrap: break-word;
 
   .comercial-e {
     color: ${({ theme }) => theme.colors.primary};
   }
 
+  .last {
+    font-size: 2.3rem;
+    width: 50%;
+    margin-bottom: 2rem;
+    text-align: center;
+
+    @media (max-width: 480px) {
+      width: 100%;
+      font-size: 2rem;
+    }
+  }
   b {
     text-align: center;
   }
@@ -33,26 +46,202 @@ export const Section = styled.section<{
     font-size: 1rem;
     max-width: 50%;
   }
+
+  @media (max-width: 1024px) {
+    width: 100%;
+    font-size: 1.5rem;
+    padding: 2rem 1.5rem;
+
+    p {
+      font-size: 1rem;
+      max-width: 80%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+    padding: 1.5rem;
+
+    p {
+      font-size: 0.9rem;
+      max-width: 90%;
+    }
+  }
+
+  /* @media (max-width: 480px) {
+    font-size: 1rem;
+    padding: 1rem;
+    p {
+      font-size: 0.8rem;
+      max-width: 100%;
+    }
+  } */
 `;
 
 export const IconsRow = styled.div`
-  max-width: 768px;
   width: 100%;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 2rem;
   margin-top: 3rem;
+
+  @media (max-width: 1024px) {
+    gap: 1.5rem;
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    flex-wrap: wrap;
+  }
 `;
 
 export const IconItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  row-gap: 1rem;
-  font-size: 1rem;
+  font-size: 1.2rem;
+  width: 30%;
 
   span {
-    width: 80%;
     text-align: center;
+  }
+
+  @media (max-width: 1024px) {
+    width: 45%;
+  }
+
+  @media (max-width: 768px) {
+    width: 60%;
+  }
+
+  @media (max-width: 480px) {
+    width: 80%;
+  }
+`;
+
+export const Icon = styled.div`
+  svg {
+    width: 10vw;
+    height: auto;
+  }
+
+  @media (max-width: 1024px) {
+    svg {
+      width: 15vw;
+    }
+  }
+
+  @media (max-width: 768px) {
+    svg {
+      width: 20vw;
+    }
+  }
+
+  @media (max-width: 480px) {
+    svg {
+      width: 25vw;
+    }
+  }
+`;
+
+export const AboutSection = styled.section<{
+  $isDark?: boolean;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  padding: 4rem 8rem;
+  gap: 2rem;
+  background-color: ${({ theme, $isDark }) =>
+    $isDark ? theme.colors.black : theme.colors.white};
+  color: ${({ theme, $isDark }) =>
+    $isDark ? theme.colors.white : theme.colors.black};
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    text-align: center;
+    padding: 3rem 2rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 2rem;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 40%;
+
+  h6 {
+    text-align: left;
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 1.2rem;
+    line-height: 1.6;
+  }
+
+  @media (max-width: 1024px) {
+    max-width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+
+    h6 {
+      text-align: center;
+      font-size: 2rem;
+    }
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+
+  @media (max-width: 480px) {
+    h6 {
+      text-align: center;
+      font-size: 1.8rem;
+    }
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  flex: 0.6;
+  display: flex;
+  justify-content: center;
+`;
+export const StyledImage = styled(Image)`
+  width: 100%;
+  max-width: 500px;
+  border-radius: 1rem;
+  object-fit: contain;
+
+  @media (max-width: 1024px) {
+    max-width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    img {
+      max-width: 350px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    img {
+      max-width: 300px;
+    }
   }
 `;
